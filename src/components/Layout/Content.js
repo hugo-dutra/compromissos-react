b51b3d76c800cff4e216f66c00b89ef88a96cd16
hugo-dutra <hugo.dutra@hotmail.com>
2020-07-24 16:50:02 -0300

    Commit antes das rotas serem implementadas.

--- src/components/Layout/Content.js
+++ src/components/Layout/Content.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Content.css'
+import Telefones from '../cruds/telefones';
+import Contatos from '../cruds/contatos';
+import Compromissos from '../cruds/compromissos';
 
 export default () => {
   return (
     <div className="content">
-      <h3>Content</h3>
+      <Telefones></Telefones>
+      <Contatos></Contatos>
+      <Compromissos></Compromissos>
     </div>
 
   )
